4bb171ea93c541df6c9638b4f3c69db11e400ed4
Uwe L. Korn <uwelk@xhochy.com>
2013-11-11 10:55:34 +0000

    Rename test file

--- src/test/scala/com/xhochy/AppTest.scala
+++ src/test/scala/com/xhochy/AppTest.scala
@@ -2,11 +2,11 @@ package com.damianhelme
 import org.scalatest.FunSuite
 import org.scalatest.FeatureSpec
  class AppTest extends FunSuite {
-  
+
   test("my first test") {
-    expect("hello") {
+    assertResult("hello") {
       "hello"
     }
   }
 }
-	
+
